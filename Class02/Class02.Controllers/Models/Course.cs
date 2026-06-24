c8d5ebe3fcb1e5eae7e8fbb6c6cfbe6075145f4c
Animrymic <trojanovikdragan@gmail.com>
2026-06-24 17:40:31 +0200

    new code added

--- Class02/Class02.Controllers/Models/Course.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Class02.Controllers.Models;
-
-public class Course
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int NumberOfClasses { get; set; }
-}
