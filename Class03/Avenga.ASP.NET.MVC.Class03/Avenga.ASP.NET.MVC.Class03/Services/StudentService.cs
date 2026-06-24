8b0ae55a58fa8d164e59429e8e86dd653a10582f
Animrymic <trojanovikdragan@gmail.com>
2026-06-24 19:33:04 +0200

    new code added

---
+++ Class03/Avenga.ASP.NET.MVC.Class03/Avenga.ASP.NET.MVC.Class03/Services/StudentService.cs
@@ -0,0 +1,20 @@
+﻿using Avenga.ASP.NET.MVC.Class03.DataAccess;
+using Avenga.ASP.NET.MVC.Class03.Models.Domains;
+
+namespace Avenga.ASP.NET.MVC.Class03.Services
+{
+    public class StudentService
+    {
+        public Student GetStudentWithActiveCourse(int id)
+        {
+            var student = InMemoryDb.Students.FirstOrDefault(x => x.Id == id);
+
+            if (student == null)
+            {
+                return null;
+            }
+
+            return student;
+        }
+    }
+}
