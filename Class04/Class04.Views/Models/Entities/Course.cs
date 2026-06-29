2eb49fda55f96094b3d88b76ad93ec91cef6d5e4
Animrymic <trojanovikdragan@gmail.com>
2026-06-29 20:39:46 +0200

    class04 demo code added

---
+++ Class04/Class04.Views/Models/Entities/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace Class04.Views.Models.Entities;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int NumberOfClasses { get; set; }
+}
