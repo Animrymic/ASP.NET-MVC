3fa93e6afe106cc34dfd0a9256afb91bc77c5517
Animrymic <trojanovikdragan@gmail.com>
2026-07-03 17:39:23 +0200

    code added

--- Class04/Class04.Views/Models/Entities/Course.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Class04.Views.Models.Entities;
-
-public class Course
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int NumberOfClasses { get; set; }
-}
--- Class04/Class04.Views/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Class04.Views.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
