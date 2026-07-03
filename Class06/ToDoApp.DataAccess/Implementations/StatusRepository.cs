34903379b1114f3477044f1867a3e2deb367aed1
Animrymic <trojanovikdragan@gmail.com>
2026-07-03 20:42:19 +0200

    final code added

---
+++ Class06/ToDoApp.DataAccess/Implementations/StatusRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoApp.DataAccess.Interfaces;
+using ToDoApp.Domain;
+
+namespace ToDoApp.DataAccess.Implementations;
+
+public class StatusRepository : IRepository<Status>
+{
+    public void Create(Status entity)
+    {
+        if(entity == null)
+        {
+            throw new Exception("Status entity cannot be null");
+        }   
+       entity.Id = StaticDb.Statuses.Last().Id + 1;
+       StaticDb.Statuses.Add(entity);
+    }
+
+    public void Delete(int id)
+    {
+        Status status = StaticDb.Statuses.FirstOrDefault(s => s.Id == id);
+        if (status == null)
+        {
+            throw new Exception("Status with Id does not exist.");
+        }
+        StaticDb.Statuses.Remove(status);
+    }
+
+    public List<Status> GetAll()
+    {
+        return StaticDb.Statuses;
+    }
+
+    public Status GetById(int id)
+    {
+        var status = StaticDb.Statuses.FirstOrDefault(s => s.Id == id);
+        if (status == null)
+        {
+            throw new Exception("Status with Id does not exist.");
+
+        }
+        return status;
+    }
+
+    public void Update(Status entity)
+    {
+       if(entity == null)
+        {
+            throw new Exception("Status item cannot be null");
+        }
+        Status status = GetById(entity.Id);
+        int index = StaticDb.Statuses.IndexOf(status);
+        StaticDb.Statuses[index] = entity;
+    }
+}
